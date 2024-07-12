578df4746824bfb081440d33e6f4e041ea7d8afd
Rofy-Ray <okyereforsonr@berea.edu>
2024-07-12 19:40:16 -0400

    updated driver code to use functions framework

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const authRouter = require('./routes/auth');
 const usersRouter = require('./routes/users');
+const functions = require('@google-cloud/functions-framework');
 
 const app = express();
 
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 
-// const yauth = (req, res) => {
-//   app(req, res);
-// };
+functions.http('yauth', (req, res) => {
+  app(req, res);
+});
 
-exports.yauth = app;
+// exports.yauth = app;
 
 // app.listen(3000, () => {
 //   console.log('yAuth Microservice is listening on port 3000');
